561c517a00affe985e297fd8e709768ec73816d3
Santiago Noh <santiagocahum25@gmail.com>
2024-07-19 13:50:44 -0600

    Feat: add user repository

---
+++ ApiUsers/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace ApiUsers.Repository.Interfaces
+{
+    public interface IRepository <T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+        public Task<IEnumerable<T>> GetAllByAsync<F>(F _filter);
+
+        //Bool or id insert - update
+        public Task<int> InsertAsync(T entity);
+
+        public Task<int> UpdateAsync(T entity);
+
+        //returnar id elemento eliminado
+        public Task<bool> DeleteAsync(int id);
+
+
+        //public Task<IEnumerable<T>> GetAllAsync (CancellationToken cancellationToken);
+    }
+}
---
+++ ApiUsers/Repository/UserRepository.cs
@@ -0,0 +1,62 @@
+﻿using ApiUsers.DataBaseContext;
+using ApiUsers.Models;
+using ApiUsers.Models.Dto.Request;
+using ApiUsers.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiUsers.Repository
+{
+    public class UserRepository : IRepository<User>
+    {
+        private GeneralRepositoryContext _context;
+
+        public UserRepository(GeneralRepositoryContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            var data = await _context.Users.ToListAsync();
+
+            return data.FindAll(x => x.IsActive == 1); //asEnumerable
+        }
+
+        public async Task<IEnumerable<User>> GetAllByAsync<FilterUserDto>(FilterUserDto _filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<User> GetByUsername(string _username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> InsertAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> UpdateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class FilterResult
+        { 
+            public bool HaveError { get; set; }
+            public bool ExistRequiredFilter { get; set; }
+            public string ErrorMessage { get; set; } = string.Empty;
+            public List<User>? DataFilterd { get; set; }
+        }
+    }
+}
